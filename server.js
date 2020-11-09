c03e276a1801a1dbd0ddfd48f3921bd353c9fddb
Mayur Goyal <mayurgoyal500@gmail.com>
2020-11-10 01:44:15 +0530

    Added cart page and login register page

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ const ejs= require('ejs');
 const expressLayout =require('express-ejs-layouts');
 const PORT =process.env.PORT || 3300
 
-//app.use(expressLayout);
 app.use(express.static('public'));
 app.set('view engine','ejs');
 app.set("views",`${__dirname}/resources/views`);
+app.use(expressLayout);
 
 app.get('/',(req,res)=>{
     res.render("home");
 });
+app.get('/cart',(req,res)=>{
+    res.render("customers/cart");
+});
+app.get("/register" , (req,res)=>{
+ res.render("auth/register");
+ 
+});
+app.get("/login" , (req,res)=>{
+    res.render("auth/login");
+ });
 app.listen(PORT);
-
